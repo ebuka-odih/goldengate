b065e90e01440088858189af5a866f69f6a55f15
Gnosis <emmagab38@gmail.com>
2023-12-14 16:09:45 +0100

    commit changes

--- app/Http/Controllers/Admin/AdminDeposits.php
+++ app/Http/Controllers/Admin/AdminDeposits.php
@@ -39,7 +39,7 @@ class AdminDeposits extends Controller
         $deposit->status = 1;
         $deposit->user_id = $request->user_id;
         $deposit->save();
-        $account = Account::whereUserId($request->user_id)->get();
+        $account = Account::findOrFail($deposit->user_id);
         $account->balance += $request->amount;
         $account->save();
 
